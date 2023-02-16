96b2ba5e35280af243b782e66b3151d94e84ef2f
difference <mabasadw@gmail.com>
2023-02-16 15:25:50 +0200

    Implemented Regions controller

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("Regions")] //Or [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAllAsync(); //This returns Domain Regions
+
+            var regionsDTOs = mapper.Map<List<Models.DTOs.Region>>(regions); //Use AutoMapper to convert to DTO
+
+            return Ok(regionsDTOs);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Profiles/RegionProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionProfile: Profile
+    {
+
+        public RegionProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTOs.Region>();
+
+            //If properties are not the same
+            //CreateMap<Models.Domain.Region, Models.DTOs.Region>()
+            //    .ForMember(destination => destination.Id, options => options.MapFrom(source => source.Id));
+
+            //Reversing a map
+            //CreateMap<Models.Domain.Region, Models.DTOs.Region>().ReverseMap();
+        }
+    }
+}
